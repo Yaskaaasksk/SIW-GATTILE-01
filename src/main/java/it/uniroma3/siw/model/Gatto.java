198513f61beaf970c9567dbc5a37b4543864356e
angelicasanky <angelicasanky@gmail.com>
2026-05-20 11:56:42 +0100

    Aggiunta classe Gatto

--- src/main/java/it/uniroma3/siw/model/Gatto.java
+++ src/main/java/it/uniroma3/siw/model/Gatto.java
@@ -1,5 +1,73 @@
 package it.uniroma3.siw.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Gatto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nome;
+    private Integer eta;    
+    private String colore;
+    private String razza;
+    private Boolean adottato; 
+    
+
+    public Gatto() {
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getEta() {
+        return eta;
+    }
+
+    public void setEta(Integer eta) {
+        this.eta = eta;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+
+    public void setRazza(String razza) {
+        this.razza = razza;
+    }
+
+    public Boolean getAdottato() {
+        return adottato;
+    }
+
+    public void setAdottato(Boolean adottato) {
+        this.adottato = adottato;
+    }
 }
